b725f5a8745a0f6829db5528931391391c7dee52
MEA\Aviral.Yadav <aviral.yadav@in.ey.com>
2019-06-28 14:31:45 +0530

    linter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
-import {ageDown, ageUp, getUsers} from './store/actions';
-// import { makeStyles } from '@material-ui/core/styles'; 
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Users from './components/users/Users';
-import UserAddForm from './components/users/UserAddForm';
-import PropTypes from 'prop-types';
+import { ageDown, ageUp, getUsers } from "./store/actions";
+// import { makeStyles } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Users from "./components/users/Users";
+import UserAddForm from "./components/users/UserAddForm";
+import PropTypes from "prop-types";
 
 const classes = {
   root: {
-    padding: 20,
-  },
+    padding: 20
+  }
 };
 
 class App extends Component {
   ageUp = () => {
     this.props.onAgeUp();
+  };
+  handleText(text) {
+    this.props.addTodo(text);
   }
-  handleText(text)      {
-    this.props.addTodo(text); 
-  } 
   componentDidMount() {
     this.props.getUsersList();
   }
@@ -31,15 +31,15 @@ class App extends Component {
     return (
       <div className="App">
         <Paper style={classes.root}>
-        <Typography variant="h5" component="h3">
-        Welcome to React
-        </Typography>
-        <div className="Age-label">
-          your age: <span>{this.props.age}</span>
-        </div>
-        <button onClick={this.ageUp}>Age UP</button>
-        <button onClick={this.props.onAgeDown}>Age Down</button>
-        {/* <button onClick={this.handleText}>Save Text</button> */}
+          <Typography variant="h5" component="h3">
+            Welcome to React
+          </Typography>
+          <div className="Age-label">
+            your age: <span>{this.props.age}</span>
+          </div>
+          <button onClick={this.ageUp}>Age UP</button>
+          <button onClick={this.props.onAgeDown}>Age Down</button>
+          {/* <button onClick={this.handleText}>Save Text</button> */}
         </Paper>
         <Users userData={this.props.users} />
         <UserAddForm />
